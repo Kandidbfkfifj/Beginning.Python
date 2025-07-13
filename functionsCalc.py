83343d182c32658bbcbcc185bc5c35d3ce6e3e09
ilvy <132714716+Kandidbfkfifj@users.noreply.github.com>
2025-07-13 15:21:52 +0200

    Add files via upload
    simple calc to change the given numbers and output look inside the code/change the numbers by print

---
+++ functionsCalc.py
@@ -0,0 +1,18 @@
+def add(a, b): # this code adds, subtracts multiplys divides and exp´s the given numbers, you just need to change the print function to change the output
+  return a + b
+print(add(3,5))
+def subtract(a, b):
+  return a - b
+print(subtract(3, 5))
+def multiply(a, b):
+  return a * b
+print(multiply(3, 5))
+def divide(a, b):
+  return a / b 
+print(divide(3, 5))
+def exp(a, b):
+  return a % b 
+print(exp(3, 5))
+
+
+
